5f9dca6ce2107770e41a788ab7ae1116cf107377
morapse <ahmetyusuf.kuru@gmail.com>
2023-12-02 23:09:53 +0300

    01.12.2023-23:10

---
+++ Sarfea_New2/Sarfea_New/AFM/static/clientt.js
@@ -0,0 +1,7 @@
+document.getElementById("musteri-ac").addEventListener("click", function(){    
+        document.querySelector(".musteriWindow").style.display = "flex";    
+});
+document.getElementById("musteri-modal").addEventListener("click", function(){
+    document.querySelector(".musteriWindow").style.display = "none";
+});
+
