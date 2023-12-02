c537b35396660ccb855701ec2f1b193480d013ed
akeylmz <a.kylmz12@gmail.com>
2023-12-02 17:39:35 +0300

    2/12/2023 17:40  FormErr

--- Sarfea_New2/Sarfea_New/AFM/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AfmConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "AFM"
-
-    def ready(self):
-        import AFM.signals  # Burada signals.py dosyanızın tanıtılması
